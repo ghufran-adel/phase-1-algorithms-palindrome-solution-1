30d7c0b84f2dfa75c069a4085eb43807ea80ab99
ghufran adel <gho0ofran.adel14@gmail.com>
2022-06-07 22:30:02 +0300

    compeleted assignment

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reverse=word.split('').reverse().join('');
+  if (reverse.toLowerCase()===word.toLowerCase()){
+    return true;
+  }
+  else return false ;
 }
+
 
 /* 
   Add your pseudocode here
+ if the word equal to its revers the output should be true
+ if not output should be false
 */
 
 /*
   Add written explanation of your solution here
+  -split the word to letter in array
+  -reverse the letters
+  -join the letters to get the revrse word as string 
+  -add the condtion if the word ===to revers one or not 
 */
 
 // You can run `node index.js` to view these console logs
